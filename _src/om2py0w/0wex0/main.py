21e5434c7e0f84cf98edded3ae9883395d1839c5
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-20 16:07:03 +0800

    main beta2.0

--- _src/om2py0w/0wex0/main.py
+++ _src/om2py0w/0wex0/main.py
@@ -13,7 +13,23 @@ def main():
     for words in keywords:
         print words,
     print "\n"
+    
+    script_statement = """ # 脚本说明：
 
+    1.这是每日日志书写脚本
+    2.首先打印的是脚本所在文件夹所有日志(txt格式)名称及内容
+    3.请按照提示进行 日志书写
+    4.想要结束日志写作？最后一行请单独输入：end
+    
+    祝您书写愉快！
+    """
+    
+    print(script_statement.encode(sys.stdout.encoding) + "\n")
+    
+    read_diary()
+    write_diary()
+
+def read_diary():
     current_dir = os.getcwd() # 打印之前日志
     os.chdir(current_dir)
 
@@ -22,18 +38,7 @@ def main():
         file_content = open(file, "r")
         print file_content.read()
 
-    script_statement = """ # 脚本说明：
-
-        1.这是每日日志书写脚本
-        2.以上打印的是脚本所在文件夹所有日志(txt格式)名称及内容
-        3.请按照提示进行 日志书写
-        4.想要结束日志写作？最后一行请单独输入：end
-        
-        祝您书写愉快！
-        """
-    print(script_statement.encode(sys.stdout.encoding))
-
-    # ******* 写日志 *******
+def write_diary():
     done = False
     textInput = ""
 
@@ -53,7 +58,6 @@ def main():
     diary_writer.close()
     print ("Here is your " + inputDate +" diary: " + textInput)
 
-# ******* date time function *******
 def ask_date(prompt):  # 是否添加时间
     ok = raw_input(prompt)
     if ok in yes_list:
