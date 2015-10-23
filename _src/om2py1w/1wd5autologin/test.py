a87cd8fead049039dbf7bc1b03742a92d3bc441c
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-23 18:14:59 +0800

    encode chinese right

--- _src/om2py1w/1wd5autologin/test.py
+++ _src/om2py1w/1wd5autologin/test.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys
+reload(sys) # 必须 reload
+sys.setdefaultencoding('utf-8') # 默认编码
 
 keywords = sys.argv[1:]
 print type(keywords)  # list
 
 print (sys.argv[0]) # str
+
+your_keywords = sys.argv[1:]
+
+for keywords in your_keywords:
+    print keywords
