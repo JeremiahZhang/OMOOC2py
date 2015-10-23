9b08f35c1a3c8dc634aa2a284f8ce1972b380298
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-23 17:06:18 +0800

    google search

--- _src/om2py1w/1wd5autologin/go_google.py
+++ _src/om2py1w/1wd5autologin/go_google.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -7,21 +8,21 @@ print """ Example 1:
     
     * open a new Firefox browser
     * load the google homepage
-    * search for "github"
-    * close the browser
+    * search for "your_keywords"
     
 """
+your_keywords = sys.argv[1:]
 
 browser = webdriver.Firefox()             # open your firefox browser
 
 browser.get("http://www.google.com")      # go to google webpage
 assert 'Google' in browser.title          # confirm that title has “Google” word in it
 
-# browser.get("http://www.google.com")
-# assert 'google' in browser.title
+elem = browser.find_element_by_name('q')  # name = "q" is in the search field of the google webpage source code
 
-elem = browser.find_element_by_name('q')  # name = "q" is in the search field of the google webpage source code 
-elem.send_keys('github' + Keys.RETURN)    # search keyword Keys.RETURN like keyboard enter or Go
+for keywords in your_keywords:
+    elem.send_keys(keywords + " ")
+
+elem.send_keys(Keys.RETURN)    # search keyword Keys.RETURN like keyboard enter or Go
+
 print "Mission Completed!"
-
-# browser.quit()
---
+++ _src/om2py1w/1wd5autologin/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+
+keywords = sys.argv[1:]
+print type(keywords)  # list
+
+print type(keywords[0]) # str
