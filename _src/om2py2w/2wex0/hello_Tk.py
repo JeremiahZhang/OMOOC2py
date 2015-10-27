9227aedd676a7c943b2616df03a1fd3c16bbda67
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-27 19:53:05 +0800

    hello again TK

--- _src/om2py2w/2wex0/hello_Tk.py
+++ _src/om2py2w/2wex0/hello_Tk.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
-
+# import every thing from Tkinter
 root = Tk()
+# to initialize Tkinter need to creat a TK root widget
 
-w = Label(root, text="Hello, world!")
+w = Label(root, text="Hello, world!") # Label widget
 w.pack()
+""" # pack method
 
-root.mainloop()
+    This tells it to size itself to fit the given text,
+    and make itself visible. """
+
+root.mainloop() # start root widget
---
+++ _src/om2py2w/2wex0/hello_Tk_again.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+
+class App:
+    def __init__(self, master):
+        
+        frame = Frame(master)
+        frame.pack()
