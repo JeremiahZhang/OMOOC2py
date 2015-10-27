f1dc422e951e1e3bfe784a704b1c1595fad05fa4
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-27 20:43:47 +0800

    bind and event test

--- _src/om2py2w/2wex0/Tk_event_bind_keyboard.py
+++ _src/om2py2w/2wex0/Tk_event_bind_keyboard.py
@@ -4,4 +4,15 @@ from Tkinter import *
 root = Tk()
 
 def key(event):
-    print 
+    print "pressed", repr(event.char)
+
+def callback(event):
+    frame.focus_set()
+    print "clicked at", event.x, event.y
+
+frame = Frame(root, width=100, height=100)
+frame.bind("<Key>", key)
+frame.bind("<Button-1>", callback)
+frame.pack()
+
+root.mainloop()
