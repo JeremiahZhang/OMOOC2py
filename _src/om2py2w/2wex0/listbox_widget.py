0915ef825c740edc8602e8f42abc1d1bb733a17a
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-27 22:42:45 +0800

    listbox to print test part 01

--- _src/om2py2w/2wex0/listbox_widget.py
+++ _src/om2py2w/2wex0/listbox_widget.py
@@ -2,8 +2,15 @@
 from Tkinter import *
 
 master = Tk()
+# use list box to print the past logs
+past_logs = Listbox(master)
+past_logs.pack()
 
-listbox = Listbox(master)
+past_logs.insert(END, "Here is your past logs:--->")
+
+# 调用函数 打印过去的日志 在listbox中
+
+"""listbox = Listbox(master)
 listbox.pack()
 
 listbox.insert(END, "a list entry")
@@ -11,4 +18,4 @@ listbox.insert(END, "a list entry")
 for item in ["one", "two", "three", "four"]:
     listbox.insert(END, item)
 
-mainloop()
+mainloop()"""
