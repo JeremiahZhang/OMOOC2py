54ef51287404160de9603a6fa804abf0e9c0881a
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 10:24:50 +0800

    button test again

--- _src/om2py2w/2wex0/button_widget.py
+++ _src/om2py2w/2wex0/button_widget.py
@@ -3,10 +3,26 @@ from Tkinter import *
 
 master = Tk()
 
-def callback():
-    print "click!"
+"""b = Button(master, text="click me!", image=pattern, compound=CENTER)
+b.pack() 
+you need to define the pattern"""
 
-b = Button(master, text="OK", command=callback)
+"""b = Button(master, text="here is your diary button test!", 
+    anchor=W, justify=LEFT, padx=2)
+b.pack()"""
+
+"""f = Frame(master, height=32, width=32)
+f.pack_propagate(0) # dont shrink
+f.pack()
+
+b = Button(f, text="Sure!")
+b.pack(fill=BOTH, expand=1)"""
+
+"""def callback():
+    print "click!"""
+
+"""b = Button(master, text="OK", command=callback)
 b.pack()
-
+b = Button(master, text="Help", state=DISABLED)
+b.pack()"""
 mainloop()
