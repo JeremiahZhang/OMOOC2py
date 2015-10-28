85199cdd9a5cb68ee5297b9b034d30660ec458eb
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 14:58:47 +0800

    windows bar

--- _src/om2py2w/2wex0/app_windows.py
+++ _src/om2py2w/2wex0/app_windows.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
-
+# refer to http://effbot.org/tkinterbook/tkinter-application-windows.htm
 def callback():
     print "called the callback!"
 
@@ -20,5 +20,20 @@ filemenu.add_command(label="Exit", command=callback)
 helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=callback)
+
+# create the toolbar
+toolbar = Frame(root)
+
+b = Button(toolbar, text="new", width=6, command=callback)
+b.pack(side=LEFT, padx=2, pady=2)
+
+b = Button(toolbar, text="open", width=6, command=callback)
+b.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
+
+# status bars
+status = Label(root, text="", bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
 
 root.mainloop()
