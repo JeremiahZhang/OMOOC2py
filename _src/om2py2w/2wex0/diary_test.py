4c92b83f8974ed51c79b836823b4b67c259b87bf
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 23:03:45 +0800

    keep calm and use menu again

--- _src/om2py2w/2wex0/diary_test.py
+++ _src/om2py2w/2wex0/diary_test.py
@@ -7,7 +7,7 @@ class Application(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
+        self.createWidgets(master)
 
     def write_log(self):
 
@@ -27,7 +27,7 @@ class Application(Frame):
         # self.content_entry.bind("<Return>", self.save)
         # self.content_entry.focus_set()
 
-    def save(self, event):
+    def save(self):
         name = self.log_name.get() + ".txt"
         log_writer = open(name, "a+")
 
@@ -49,7 +49,7 @@ class Application(Frame):
             self.past_logs.insert(END, "Log_content:---> " + file_content.read() + "\n \n")
             file_content.close()
 
-    def createWidgets(self):
+    def createWidgets(self, master):
 
         self.scrollbar = Scrollbar(self, orient=VERTICAL) # scroll bar
         self.past_logs = Text(self, width=100, height=20, 
@@ -58,19 +58,20 @@ class Application(Frame):
 
         self.scrollbar.pack(side=RIGHT, fill=Y)
         self.past_logs.pack()
-        # buttons
-        self.print_button = Button(self, text="PrintLogs", width=10, command=self.print_log)
-        self.print_button.pack()
 
-        self.write_button = Button(self, text="Write", width=10, command=self.write_log)
-        self.write_button.pack()
-        
-        self.save_button = Button(self, text="Save", width=10, command=self.save)
-        self.save_button.pack()
+        self.menu = Menu(self)
+        master.config(menu=self.menu)
 
-        self.quit_button = Button(self, text="Quit", fg="red", width=10, command=self.cancel)
-        self.quit_button.pack()
+        self.filemenu = Menu(self)
+        self.menu.add_cascade(label="file", menu=self.filemenu)
+        self.filemenu.add_command(label="PastLogs", command=self.print_log)
+        self.filemenu.add_command(label="New", command=self.write_log)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="Exit", command=self.cancel)
 
+        self.helpmenu = Menu(self)
+        self.menu.add_cascade(label="Help", menu=self.helpmenu)
+        self.helpmenu.add_command(label="Guide", command=self.readme)
 
     def cancel(self):
         self.quit()
@@ -93,17 +94,25 @@ if __name__ == '__main__':
     main()
 
 """
-menu = Menu(frame1)
-master.config(menu=menu)
 
-filemenu = Menu(frame1)
-menu.add_cascade(label="file", menu=filemenu)
-filemenu.add_command(label="PastLogs", command=read_diary)
-filemenu.add_command(label="New", command=write_diary)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=cancel)
-
-helpmenu = Menu(frame1)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="Guide", command=readme)
 """
+
+"""        self.scrollbar = Scrollbar(self, orient=VERTICAL) # scroll bar
+        self.past_logs = Text(self, width=100, height=20, 
+            yscrollcommand=self.scrollbar.set)              # past logs
+        self.scrollbar.config(command=self.past_logs.yview)
+
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.past_logs.pack()
+        # buttons
+        self.print_button = Button(self, text="PrintLogs", width=10, command=self.print_log)
+        self.print_button.pack()
+
+        self.write_button = Button(self, text="Write", width=10, command=self.write_log)
+        self.write_button.pack()
+        
+        self.save_button = Button(self, text="Save", width=10, command=self.save)
+        self.save_button.pack()
+
+        self.quit_button = Button(self, text="Quit", fg="red", width=10, command=self.cancel)
+        self.quit_button.pack()"""
