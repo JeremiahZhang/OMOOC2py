f8fdd4db5f0aab939681151bf9d7e8cdab3815ee
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 14:35:38 +0800

    scrll bar text

--- _src/om2py2w/2wex0/diary_test.py
+++ _src/om2py2w/2wex0/diary_test.py
@@ -6,23 +6,29 @@ master = Tk()
 
 frame = Frame(master)
 frame.master.title("Writing for Loving, Learning and Sharing")
-frame.master.maxsize(1000,400)  # set the size
+# frame.master.maxsize(1000,400)  # set the size
 frame.pack()
 
-diary_name_content = StringVar()
-Label(master, text="Diary Name->").pack(side=TOP)
-diary_name_content_entry = Entry(master, textvariable=diary_name_content)
-diary_name_content_entry.pack()
+# 打印先前的日志 使用 Text widget
+scrollbar = Scrollbar(master, orient=VERTICAL)
+past_logs = Text(master, width=100, height=20, 
+    yscrollcommand=scrollbar.set)
+scrollbar.config(command=past_logs.yview)
+scrollbar.pack(side=RIGHT, fill=Y)
 
-diary_content = StringVar()
-Label(master, text="Write Words->").pack(side=TOP)
-diary_content_entry = Entry(master, textvariable=diary_content)
-diary_content_entry.pack()
-
-# past_logs_text = Text(master)
-# past_logs_text.pack()
+past_logs.pack()
 
 def write_diary():
+    # 输入文字部分 使用entry
+    diary_name_content = StringVar()
+    Label(master, text="Diary Name->").pack(side=TOP)
+    diary_name_content_entry = Entry(master, textvariable=diary_name_content)
+    diary_name_content_entry.pack()
+
+    diary_content = StringVar()
+    Label(master, text="Write Words->").pack(side=TOP)
+    diary_content_entry = Entry(master, textvariable=diary_content)
+    diary_content_entry.pack()
 
     diary_name = diary_name_content.get() + ".txt"
     diary_writer = open(diary_name, "w")
@@ -32,31 +38,25 @@ def write_diary():
     diary_writer.close()
 
 def read_diary():
-    scrollbar = Scrollbar(master, orient=VERTICAL)
-    past_logs = Text(master, width=100, height=20, 
-        yscrollcommand=scrollbar.set)
-    scrollbar.config(command=past_logs.yview)
-    scrollbar.pack(side=RIGHT, fill=Y)
 
-    past_logs.pack()
-    past_logs.insert(END, "Here is your past logs:--->")
+    past_logs.insert(END, "Here is your past logs:--->" + "\n")
 
     current_dir = os.getcwd() # 打印之前日志
     os.chdir(current_dir)
     
     for file in glob.glob("*.txt"):
-        past_logs.insert(END, "Log_name:--->" + file)
+        past_logs.insert(END, "Log_name:--->" + file + "\n")
         file_content = open(file, "r")
-        past_logs.insert(END, "Log_content:---> " + file_content.read() + "\n")
+        past_logs.insert(END, "Log_content:---> " + file_content.read() + "\n \n")
         file_content.close()
 
 quit_button = Button(master, text="Quit", fg="red", width=10, command=master.quit)
-quit_button.pack(side=BOTTOM)
+quit_button.pack()
 
 past_logs_button = Button(master, text="PrintLogs", fg="blue", width=10, command=read_diary)
-past_logs_button.pack(side=BOTTOM)
+past_logs_button.pack()
 
 write_button = Button(master, text="Write", width=10, command=write_diary)
-write_button.pack(side=BOTTOM)
+write_button.pack()
 
 mainloop()
