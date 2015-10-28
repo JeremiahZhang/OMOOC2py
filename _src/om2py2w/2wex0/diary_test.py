e4031ba776b0c916ae0199911b58e01314b426eb
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 09:14:33 +0800

    diary frame finish

---
+++ _src/om2py2w/2wex0/diary_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from Tkinter import *
+import sys, os, glob
+
+master = Tk()
+
+frame = Frame(master)
+frame.master.title("Writing for Loving, Learning and Sharing")
+frame.master.maxsize(500,200)  # set the size
+frame.pack()
+
+mainloop()
--- _src/om2py2w/2wex0/main.py
+++ _src/om2py2w/2wex0/main.py
@@ -4,16 +4,21 @@ from Tkinter import *
 def main():
 
     class App:
-        """docstring for Diary_App"""
-        def __init__(self, master):
+        """Dear here is your diary system! Start writing!--->"""
+        def __init__(self,master=None):
 
             frame = Frame(master)
             frame.pack()
-            
+
 # GUI 标题
-    my_diary_app = App()
-    my_diary_app.master.title("Writing for Loving, Learning and Sharing")
-    my_diary_app.master.maxsize(1000,400)
+    diary = Tk()
+    diary_app = App(master=diary)
+
+    diary_app.master.title("Writing for Loving, Learning and Sharing")
+    diary_app.master.maxsize(1000,400)
+
+    diary_app.mainloop()
+    diary.destroy()
 
 if __name__ == '__main__':
     main()
