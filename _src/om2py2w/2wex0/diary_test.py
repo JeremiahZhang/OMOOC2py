b569266bd54597022dced6224684fd17db77f509
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 11:09:02 +0800

    write diary able

--- _src/om2py2w/2wex0/diary_test.py
+++ _src/om2py2w/2wex0/diary_test.py
@@ -9,6 +9,25 @@ frame.master.title("Writing for Loving, Learning and Sharing")
 frame.master.maxsize(500,200)  # set the size
 frame.pack()
 
+diary_content = StringVar()
+diary_write_entry = Entry(master, textvariable=diary_content)
+diary_write_entry.pack
+
+def write_diary():
+    diary_name_content = StringVar()
+    diary_name_content_entry = Entry(master, textvariable=diary_name_content)
+    diary_name_content_entry.pack()
+    
+    diary_name = diary_name_content.get() + ".txt"
+    diary_writer = open(diary_name, "w")
+    
+    diary_content = StringVar()
+    diary_content_entry = Entry(master, textvariable=diary_content)
+    diary_content_entry.pack()
+
+    diary_writer.write(diary_content.get())
+    diary_writer.close()
+
 def read_diary():
 
     past_logs = Listbox(master, width=100, height=20)
@@ -24,10 +43,13 @@ def read_diary():
         past_logs.insert(END, "Log_content:---> " + file_content.read() + "\n")
         file_content.close()
 
-past_logs_button = Button(master, text="PrintLogs", fg="blue", width=10, command=read_diary)
-past_logs_button.pack(side=TOP)
-
 quit_button = Button(master, text="Quit", fg="red", width=10, command=master.quit)
-quit_button.pack(side=TOP)
+quit_button.pack(side=BOTTOM)
+
+past_logs_button = Button(master, text="PrintLogs", fg="blue", width=10, command=read_diary)
+past_logs_button.pack(side=BOTTOM)
+
+write_button = Button(master, text="Write", width=10, command=write_diary)
+write_button.pack(side=BOTTOM)
 
 mainloop()
