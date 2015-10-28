c511cc50cb05fe8deabf0d5ac3eac7e39e9258b0
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-28 12:28:09 +0800

    diary name entry done

--- _src/om2py2w/2wex0/diary_test.py
+++ _src/om2py2w/2wex0/diary_test.py
@@ -9,23 +9,20 @@ frame.master.title("Writing for Loving, Learning and Sharing")
 frame.master.maxsize(500,200)  # set the size
 frame.pack()
 
+diary_name_content = StringVar()
+Label(master, text="Diary Name->").pack(side=TOP)
+diary_name_content_entry = Entry(master, textvariable=diary_name_content)
+diary_name_content_entry.pack()
+
 diary_content = StringVar()
-diary_write_entry = Entry(master, textvariable=diary_content)
-diary_write_entry.pack
+Label(master, text="Write Words->").pack(side=TOP)
+diary_content_entry = Entry(master, textvariable=diary_content)
+diary_content_entry.pack()
 
 def write_diary():
-    diary_name_content = StringVar()
-    Label(master, text="Diary Name->").pack(side=TOP)
-    diary_name_content_entry = Entry(master, textvariable=diary_name_content)
-    diary_name_content_entry.pack()
-    
+
     diary_name = diary_name_content.get() + ".txt"
     diary_writer = open(diary_name, "w")
-    
-    diary_content = StringVar()
-    Label(master, text="Write Words->").pack(side=TOP)
-    diary_content_entry = Entry(master, textvariable=diary_content)
-    diary_content_entry.pack()
 
     diary_writer.write(diary_content.get())
     diary_writer.close()
@@ -51,7 +48,7 @@ quit_button.pack(side=BOTTOM)
 past_logs_button = Button(master, text="PrintLogs", fg="blue", width=10, command=read_diary)
 past_logs_button.pack(side=BOTTOM)
 
-write_button = Button(master, text="Write", width=10, command=write_diary)
-write_button.pack(side=BOTTOM)
+# write_button = Button(master, text="Write", width=10, command=write_diary)
+# write_button.pack(side=BOTTOM)
 
 mainloop()
