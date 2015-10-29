c963be4aae504feeb4d00d4934a64f4ead004184
JeremiahZhang <zhangleisuda@gmail.com>
2015-10-29 15:40:25 +0800

    text.get() error

--- _src/om2py2w/2wex0/diary_test.py
+++ _src/om2py2w/2wex0/diary_test.py
@@ -9,56 +9,68 @@ class Application(Frame):
         self.pack()
         self.createMenus(master)
 
-    def write_log(self):
+    def createScrollbar(self):
+        self.scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
 
-        self.past_logs.pack_forget()
-        self.scrollbar.pack_forget()
-
-        self.log_name = StringVar()
-        Label(self, text="Diary Name->").pack(side=TOP)
-        self.name_entry = Entry(self, textvariable=self.log_name) # here input your diary name
-        self.name_entry.pack()
-
-        Label(self, text="Write Words->").pack(side=TOP)
-        self.log_content = StringVar()
-        self.content_entry = Entry(self, width=100, textvariable=self.log_content)                            # input your words
-        self.content_entry.pack()
-
-        # self.content_entry.bind("<Return>", self.save)
-        # self.content_entry.focus_set()
-
-    def save(self):
-        name = self.log_name.get() + ".txt"
-        log_writer = open(name, "a+")
-
-        log_writer.write(self.log_content.get())
-        log_writer.close()
+    def createText(self):
+        self.text = Text(self, width=1000, height=400,
+            yscrollcommand=self.scrollbar.set)
+        self.text.focus_force() #  keybord 光标在Text上
+        self.text.pack()
+        self.scrollbar.config(command=self.text.yview)
 
     def printLogs(self):
 
         self.createScrollbar()
         self.createText()
 
-        self.past_logs.insert(END, "Here is your past logs:--->" + "\n")
+        self.text.insert(END, "Here is your past logs:--->" + "\n")
 
         current_dir = os.getcwd()       # print past logs
         os.chdir(current_dir)
     
         for file in glob.glob("*.txt"):
-            self.past_logs.insert(END, "Log_name:--->" + file + "\n")
+            self.text.insert(END, "Log_name:--->" + file + "\n")
             file_content = open(file, "r+")
-            self.past_logs.insert(END, "Log_content:---> " + file_content.read() + "\n \n")
+            self.text.insert(END, "Log_content:---> " + file_content.read() + "\n \n")
             file_content.close()
 
-    def createScrollbar(self):
-        self.scrollbar = Scrollbar(self, orient=VERTICAL)
-        self.scrollbar.pack(side=RIGHT, fill=Y)
+    def newLog(self):
 
-    def createText(self):
-        self.past_logs = Text(self, width=100, height=20, 
-            yscrollcommand=self.scrollbar.set)
-        self.past_logs.pack()
-        self.scrollbar.config(command=self.past_logs.yview)
+        self.text.pack_forget()
+        self.scrollbar.pack_forget()
+
+        self.createScrollbar()
+        self.createText()
+
+    def dialog(self):
+
+        top = self.top = Toplevel()
+
+        Label(top, text="Input your log name->").grid(row=0)
+        
+        self.log_name = Entry(top)
+        self.log_name.focus_force()
+        self.log_name.grid(row=0, column=1)
+
+        b = Button(top, text="OK", command=self.ok)
+        b.grid(row=1)
+    
+    def ok(self):
+        self.name = self.log_name.get() + ".txt"
+        self.top.destroy()
+
+    def save(self):
+
+        self.dialog()
+        self.wait_window(self)
+
+        log_writer = open(self.name, "a+")
+
+        self.content = self.text.get(1.0, END)
+        log_writer.write(self.content)
+        log_writer.close()
 
     def createMenus(self, master):
 
@@ -68,7 +80,8 @@ class Application(Frame):
         self.filemenu = Menu(self)
         self.menu.add_cascade(label="file", menu=self.filemenu)
         self.filemenu.add_command(label="PastLogs", command=self.printLogs)
-        self.filemenu.add_command(label="New", command=self.write_log)
+        self.filemenu.add_command(label="New", command=self.newLog)
+        self.filemenu.add_command(label="Save", command=self.save)
         self.filemenu.add_separator()
         self.filemenu.add_command(label="Exit", command=self.cancel)
 
@@ -77,7 +90,7 @@ class Application(Frame):
         self.helpmenu.add_command(label="Guide", command=self.readme)
 
     def cancel(self):
-        self.quit()
+        self.destroy()
 
     def readme():
         pass
