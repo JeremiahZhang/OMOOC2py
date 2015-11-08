d1b8b7782d9cca5cf5c9d50d1871d7a739da2dc7
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-08 15:52:16 +0800

    todo use GET values

--- _src/om2py4w/todo/todo.py
+++ _src/om2py4w/todo/todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run, template
+from bottle import route, run, template, request
 
 @route('/todo')
 @route('/my_todo_list')
@@ -11,5 +11,24 @@ def todo_list():
     c.close()
     output = template('make_table', rows=result)
     return output
+
+@route('/new', method='GET')
+def new_item():
+
+    if request.GET.get('save','').strip():
+
+        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
 
 run(host='localhost', port=8090, debug=True, reload=True)
