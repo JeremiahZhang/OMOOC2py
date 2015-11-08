30c8da0517455d7ced0f6a8d3a9fd5faba31ab0b
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-08 22:30:22 +0800

    down bottle.py

--- _src/om2py4w/4wex0/demoBottle01.py
+++ _src/om2py4w/4wex0/demoBottle01.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 from bottle import route, run, request, template
+import sys
 
 @route('/')  # here you can type http://localhost:8080 to see myDiary.log
 @route('/hello')
+
 def hello():
     return """
     <body>
@@ -15,28 +17,15 @@ def hello():
         </ul>
     </body>"""
 
-@route('/read')
-def read_diary():
-    pass
-
-@route('/write')
+@route('/write', method='GET')
 def write_diary():
-
     if request.GET.get('save','').strip():
-
         diary_words = request.GET.get('words','')
-
-        diary_name = 'myDiary.log' # if not exist then creat
+        diary_name = 'Diary.log' # if not exist then creat
         diary_file = open(diary_name, 'a+')
         diary_file.write(diary_words + '\n')
         diary_file.close()
-
     else:
-        return template('write_diary.tpl')
+        return template('write_words.tpl')
 
-@route('/static/<filename>') # without the line 4 you must type http://localhost:8080/static/myDiary.log to brower can see myDiary.log file
-def server_static(filename="myDiary.log"):
-    return static_file(filename, root='/home/jeremiahzhang/OMOOC2py/_src/om2py4w/4wex0')
-
-run(host='localhost', port=8080, debug=True, reloader=1)
-# switched debug off for publich applocations
+run(host='localhost', port=8010, debug=True, reloader=1) # switched debug off for publich applocations
