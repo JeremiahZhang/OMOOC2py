ee6db956785e491c41860b20a29fcf3861a00d66
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-08 12:09:12 +0800

    demo 1 HTTP Request Methods usrname pwd

--- _src/om2py4w/4wex0/demoBottle01.py
+++ _src/om2py4w/4wex0/demoBottle01.py
@@ -1,7 +1,27 @@
 # coding=utf-8
-from bottle import route, run, template
+from bottle import route, get, post, request, run
 
-@route('/hello/<name>')
+@get('/login') # or @route('/login')
+def login():
+    return """
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="login" type="submit" />
+            """
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+
+
+"""@route('/hello/<name>')
 def greet(name='Stranger'):
     return template('Hello {{name}}, how are you?', name=name)
 
@@ -11,11 +31,13 @@ def show_wiki_page(pagename):
 
 @route('/<action>/<user>')  # matches /follow/defnull
 def user_api(action, user):
-    pass
+    pass"""
 
 """
 def hello():
     return "Jeremiah Hello!"
 """
+
 run(host='localhost', port=8080, debug=True)
+
 # switched debug off for publich applocations
