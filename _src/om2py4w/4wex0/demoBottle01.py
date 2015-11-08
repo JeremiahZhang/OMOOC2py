042c21fc695305fcc26c77bc8ebefc377474bd68
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-08 11:41:05 +0800

    demo 1 multi route

--- _src/om2py4w/4wex0/demoBottle01.py
+++ _src/om2py4w/4wex0/demoBottle01.py
@@ -1,9 +1,14 @@
 # coding=utf-8
-from bottle import route, run
+from bottle import route, run, template
 
-@route('/hello/')
+@route('/')
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+"""
 def hello():
     return "Jeremiah Hello!"
-
+"""
 run(host='localhost', port=8080, debug=True)
 # switched debug off for publich applocations
