9f096186937dba97448e4830147c65e5d508958b
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-09 00:40:56 +0800

    can show history logs

--- _src/om2py4w/4wex0/demoBottle01.py
+++ _src/om2py4w/4wex0/demoBottle01.py
@@ -25,21 +25,14 @@ def input_diary():
         diary_name = 'Diary.log' # if not exist then creat
         diary_file = open(diary_name, 'a+')
         diary_file.write(diary_words + '\n')
+        new_content = diary_file.read()
         diary_file.close()
-        return template('write_words.tpl')
+        return template('write_words.tpl', content=new_content)
     else:
-        return template('write_words.tpl')
-
-"""@route('/write', method='GET')
-def write_diary_into_file():
-
-        diary_words = request.GET.get('words','')
-        diary_name = 'Diary.log' # if not exist then creat
-        diary_file = open(diary_name, 'a+')
-        diary_file.write(diary_words + '\n')
-        diary_file.close()
-
-        if diary_words:
-            return template('write_words.tpl')"""
+        diary_name = 'Diary.log'
+        original_file = open(diary_name, 'r')
+        original_content = original_file.read()
+        original_file.close()
+        return template('write_words.tpl', content=original_content)
 
 run(host='localhost', port=8010, debug=True, reloader=1) # switched debug off for publich applocations
--- _src/om2py4w/4wex0/write_words.tpl
+++ _src/om2py4w/4wex0/write_words.tpl
@@ -1,5 +1,8 @@
-<p>Write new words into myDiary.log:</p>
+<p>Write new words into your diary.log:</p>
 <form action="/write" method="GET">
-<input type="text" size="100" maxlength="100" name="words">
+<input type="text" size="100" maxlength="100" name="words" autofocus>
 <input type="submit" name="save" value="save">
 </form>
+
+<p> Here is your diary content! Darling!</p>
+<textarea rows="30" cols="100">{{ content }}</textarea>
