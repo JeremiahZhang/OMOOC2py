8d866fe57904b4c1e45a6eab144dd5a52f8c501a
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-09 16:40:21 +0800

    cli read html

--- _src/om2py4w/4wex0/CLI.py
+++ _src/om2py4w/4wex0/CLI.py
@@ -1,2 +1,14 @@
-# coding:utf-8
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import pycurl
+from StringIO import StringIO
 
+buffer = StringIO()
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://localhost:8010/')
+c.setopt(c.WRITEDATA, buffer)
+c.perform()
+c.close()
+
+items = buffer.getvalue()
+print (items)
