131083728746ae4293d71375413b472eb63d535a
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-16 20:17:46 +0800

    jinja2 demo 4 dynamic paremeter failed

--- _src/om2py5w/jinja2/run.py
+++ _src/om2py5w/jinja2/run.py
@@ -7,7 +7,7 @@ app = Bottle()
 
 @app.route("/")
 def template_test():
-    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5])
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], name="Je")
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8010, debug=True, reloader=1)
