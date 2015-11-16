4fee26bcd8ffeff6e0a2860c84920d68b45f16b8
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-16 21:19:58 +0800

    jinja2 demo 7 time filter failed

--- _src/om2py5w/jinja2/run.py
+++ _src/om2py5w/jinja2/run.py
@@ -1,10 +1,18 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
-from bottle import Bottle, run, route, jinja2_template, request
+from bottle import Bottle, run, route, jinja2_template, request, jinja2_env
 import jinja2
+import datetime
 
 app = Bottle()
 
+@app.template_filter()
+def datetimefilter(value, format='%Y/%m/%d %H:%M'):
+    """convert a datetime to a different format."""
+    return value.strftime(format)
+
+app.jinja2_env.filters['datetimefilter'] = datetimefilter
+
 @app.route("/")
 def template_test():
-    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5])
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4], current_time=datetime.datetime.now())
