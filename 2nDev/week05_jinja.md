d986fee3da52553fbe8df1452d21fd7d738385d5
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-16 20:43:27 +0800

    jinja2 demo 5 macro failed

---
+++ 2nDev/week05_jinja.md
@@ -0,0 +1,3 @@
+# Jinja2 初体验
+
+https://realpython.com/blog/python/primer-on-jinja-templating/
--- _src/om2py5w/jinja2/run.py
+++ _src/om2py5w/jinja2/run.py
@@ -1,13 +1,25 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
-from bottle import Bottle, run, route, jinja2_template
+from bottle import Bottle, run, route, jinja2_template, request
 import jinja2
 
 app = Bottle()
 
 @app.route("/")
 def template_test():
-    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], name="Je")
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Home")
+
+@app.route('/home')
+def home():
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Home")
+
+@app.route('/about')
+def about():
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="ABOUT")
+
+@app.route('/contact')
+def contact():
+    return jinja2_template('template.html', my_string='Wheeee!', my_list=[0, 1, 2, 3, 4, 5], title="Contact")
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8010, debug=True, reloader=1)
