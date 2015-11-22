6d776a3e75c7b28e660b528ef22205f8171b3687
JeremiahZhang <zhangleisuda@gmail.com>
2015-11-22 22:31:47 +0800

    set message to wechat if people input h

--- _src/om2py6w/6wex0/sae/web/mana4api.py
+++ _src/om2py6w/6wex0/sae/web/mana4api.py
@@ -2,6 +2,7 @@
 import sae
 from bottle import *
 from config import CFG
+import time
 
 import xml.etree.ElementTree as ET
 
@@ -18,7 +19,16 @@ APP = Bottle()
 # message wechat
 @APP.post('/echo/')
 def wechat_post():
-    # print request.forms.keys()[0]
+    # print request.forms.keys()[0] # get  xml message from wechat setdata
     xml = ET.XML(request.forms.keys()[0])
-    fromUser = xml.findtext("")
-    print xml.findtext("Content")
+    fromUser = xml.findtext("ToUserName")
+    toUser = xml.findtext("FromUserName")
+    __MsgTpye = xml.findtext("MsgType")
+    __Content = xml.findtext("Content")
+    if "text" == __MsgTpye:
+        if "h" == __Content:
+            tStamp = time.time()
+            content = "Haliluya! Welcome!是也乎!"
+            print CFG.TPL_TEXT% locals()                    # in sae debug can print
+            return CFG.TPL_TEXT% locals()                  # Use CFG in config.py, post to wechat
+    # print xml.findtext("Content")
