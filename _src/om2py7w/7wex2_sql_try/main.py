1a6244dd36c463a7aa7fca35a4ce90439e4e36a5
JeremiahZhang <zhangleisuda@gmail.com>
2015-12-02 17:13:46 +0800

    execute many and fetall"

--- _src/om2py7w/7wex2_sql_try/main.py
+++ _src/om2py7w/7wex2_sql_try/main.py
@@ -13,9 +13,20 @@ c.execute("INSERT INTO stocks Values ('2015-12-02', 'BUY', 'RHAT', 100, 35.14)")
 # save or ci the changes
 conn.commit()"""
 
+"""# do this
 t = ('RHAT',)
 c.execute('SELECT * FROM stocks WHERE symbol=?', t)
-print c.fetchone()
+print c.fetchone()"""
 
+# Larger example that inserts many records at a time
+
+purchases = [('2015-12-03', 'BUY', 'IBM', 1000, 45.00),
+('2015-12-03', 'BUY', 'MSFT', 1000, 72.00),
+('2015-12-04', 'SELL', 'IBM', 500, 53.00),
+                      ]
+
+c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
+print '*'*10
+print c.fetchall()
 # close
 conn.close()
