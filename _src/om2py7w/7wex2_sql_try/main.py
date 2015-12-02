9c7fe7c929c0dac62189a88585042c11f8a3e527
JeremiahZhang <zhangleisuda@gmail.com>
2015-12-02 16:59:16 +0800

    creat sql db

---
+++ _src/om2py7w/7wex2_sql_try/main.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import sqlite3
+conn = sqlite3.connect("example.db")
+c = conn.cursor()
+
+# create table
+c.execute("CREATE TABLE stocks \
+    (date text, trans text, symbol text, qty real, price real)")
+
+# insert a row of data refer to the execute above
+c.execute("INSERT INTO stocks Values ('2015-12-02', 'BUY', 'RHAT', 100, 35.14)")
+
+# save or ci the changes
+conn.commit()
+
+# close
+conn.close()
