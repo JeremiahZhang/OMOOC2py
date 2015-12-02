be82075dfc2cac2aeedef3c3fe309fc552150bd6
JeremiahZhang <zhangleisuda@gmail.com>
2015-12-02 16:29:21 +0800

    diary module

---
+++ _src/om2py7w/7wex1/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#qpy:2
+#qpy:webapp:Mydiary APP
+#qpy://localhost:8000/
+'''
+Diary APP of @Jeremiah Zhang
+Email: zhangleisuda@gmail.com
+版本自述 Version 1.0
+
+'''
+#  全局引用
+import sys
+#  全局变量
+PATH = "/path/2/work dir"
+#  函式撰写区
+def foo():
+    pass
+    return None
+
+#  自检区
+if __name__ == '__main__':
