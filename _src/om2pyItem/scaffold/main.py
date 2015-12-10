89788dfe3b01fec8bea45583f7b4eed8fd12c912
JeremiahZhang <zhangleisuda@gmail.com>
2015-12-10 19:50:27 +0800

    add picture 1

--- _src/om2pyItem/scaffold/main.py
+++ _src/om2pyItem/scaffold/main.py
@@ -62,9 +62,13 @@ app = Bottle()
 def index():
     return jinja2_template('index.html')
 
-@app.route('/upload', method='POST')
-def do_upload():
-    category = request.forms.get('category')
+@app.route('/pic1')
+def pic1():
+    return jinja2_template('upload1.html')
+
+@app.route('/upload1', method='POST')
+def do_upload_pic1():
+    category = "image"
     upload = request.files.get('upload')
     name, ext = os.path.splitext(upload.filename)
     if ext not in ('.png', '.jpg', '.jpeg'):
@@ -76,7 +80,7 @@ def do_upload():
 
     file_path = "{path}/{file}".format(path=save_path, file=upload.filename)
     upload.save(file_path)
-    return "File successfully saved to '{0}'.".format(save_path)
+    return file_path
 
 app.route('/__exit', method=['GET', 'HEAD'])(__exit)
 app.route('/__ping', method=['GET', 'HEAD'])(__ping)
